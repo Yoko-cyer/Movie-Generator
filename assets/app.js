79a574850b90b811a57cbbb54282805bbc72c13c
timmobrien <timmobrien@icloud.com>
2022-06-21 19:15:27 +0800

    Planning JS code

---
+++ assets/app.js
@@ -0,0 +1,22 @@
+// When the user visits the web page
+
+// We wait for them to click the generate button
+
+// when the user clicks the generate button
+
+// For loop x3
+// randomly select a movie from the catalogue
+// Create variables for movie name, metaScore, and overview
+// Display each movie in its card
+    // Movie title
+    // Movie metaScore
+    // Movie overview
+
+// When the user clicks on a movie
+// The movie title is sent to to watchmode api
+// Links for each video provider are retrieved
+
+// Create a clickable card for each provider
+    // provider name
+    // provider logo
+
